c1cdfb64801000aa89646b7381f74f68560354fc
scherichel <scherichel@gmail.com>
2014-05-28 10:55:23 -0400

    move scripts to core.js

---
+++ js/core.js
@@ -0,0 +1,7 @@
+ $(document).foundation();
+
+ $.backstretch([
+ 	"http://37.media.tumblr.com/e57c423d3d247b4c1a0bc92a77364d39/tumblr_n5rb09ND8n1qzzelmo1_1280.png"
+    , "http://37.media.tumblr.com/199b1b986b62f09d53de235b2ce40ad2/tumblr_n5pbzmDRQ31qzzelmo1_1280.png"
+    , "http://31.media.tumblr.com/38edcd553f24eddefcdb1f409e1048a0/tumblr_n5rq14QI6Y1qzzelmo1_1280.png"
+  ], {duration: 3000, fade: 750});
